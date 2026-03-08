a0eb2ed06ed07eb4bb54c6df9b31619de75f5e78
Vedant Vankar <vedant@srv1418424.hstgr.cloud>
2026-03-08 12:23:45 +0000

    fix: Firebase migration, schema updates, Docker build args

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -3,17 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCWbMn0rcb1oUyseHTrzG6Qp1lxTGgC1B0",
-    authDomain: "gym-website-01.firebaseapp.com",
-    projectId: "gym-website-01",
-    storageBucket: "gym-website-01.firebasestorage.app",
-    messagingSenderId: "798530599900",
-    appId: "1:798530599900:web:4e2b79d1dce23378956d19",
-    measurementId: "G-PCLQTKN93Y"
+    apiKey: "AIzaSyB9jjZJIpGal8rZOBnXTgVUw9v44ey5i6w",
+    authDomain: "gymato-c9660.firebaseapp.com",
+    projectId: "gymato-c9660",
+    storageBucket: "gymato-c9660.firebasestorage.app",
+    messagingSenderId: "78336517175",
+    appId: "1:78336517175:web:28ac6af2a08394f3b9cbb2",
+    measurementId: "G-Y82GZ5R330"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
-
 export default app;
